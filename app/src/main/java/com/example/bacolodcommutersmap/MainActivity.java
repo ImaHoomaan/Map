50867895845c4566cb5ab93867f0c868b54b4579
Ben <benreyd69@gmail.com>
2023-04-18 13:45:48 +0800

    Mapmapmap

---
+++ app/src/main/java/com/example/bacolodcommutersmap/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bacolodcommutersmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this ,MapsActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2500);
+
+
+
+    }
+}
